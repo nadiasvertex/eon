01bd71eb63eec0f640e1bdeff6387d6de4aa0d30
Christopher Nelson <nadiasvertex@gmail.com>
2014-04-11 22:02:32 -0400

    Work on tuple mover driver.

---
+++ engine/mover/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package performs data movement between the warm store and the cold store. The cold store is where data that is not
+written to frequently lives. Data which changes frequently lives in the warm store.
+"""
+__author__ = 'Christopher Nelson'
+
+
+
---
+++ engine/mover/postgres_monet_driver.py
@@ -0,0 +1,17 @@
+import subprocess
+
+class Driver:
+    def __init__(self, database_name):
+        self.database_name = database_name
+
+    def move_to_cold(self, table_name, predicate):
+        extract_query = "SELECT * FROM %s WHERE %s" % (table_name, predicate)
+        options = ["DELIMITER '|'"]
+        format_str = """psql -d {database} -c "COPY ({query}) TO STDOUT WITH {options}" | mclient -d {database} -s "COPY INTO {table} FROM STDIN" -"""
+        cmd = format_str.format(database=self.database_name,
+                                table=table_name,
+                                query=extract_query,
+                                options=" ".join(options))
+
+        subprocess.check_call(cmd, shell=True)
+
