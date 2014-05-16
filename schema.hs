a0a937cd0c7b67f38acc821af4a82433fce3029f
Christopher Nelson <csnelson@bethel.jw.org>
2014-05-16 16:32:14 -0400

    Added a couple of functions to adjust the column state.

--- schema.hs
+++ schema.hs
@@ -28,12 +28,30 @@ data Table = Table {
 is_column_name :: String -> Column -> Bool
 is_column_name name column = (columnName column) == name
 
-get_column_with_name :: String -> [Column] -> Maybe Column
-get_column_with_name name column_list = find (is_column_name name) column_list
+alter_column_type :: String -> ColumnType -> Column -> Column
+alter_column_type name new_column_type column =
+   case (is_column_name name column) of
+      True  -> Column (columnId column) (columnName column) new_column_type
+      False -> column
+
+alter_column_name :: String -> String -> Column -> Column
+alter_column_name name new_column_name column =
+   case (is_column_name name column) of
+      True  -> Column (columnId column) new_column_name (columnType column)
+      False -> column
+
+getColumnByName :: String -> [Column] -> Maybe Column
+getColumnByName name column_list = find (is_column_name name) column_list
 
 getColumnType :: String -> [Column] -> Maybe ColumnType
-getColumnType name column_list = let the_column = get_column_with_name name column_list
-                                 in case the_column of
-                                    Nothing   -> Nothing
-                                    otherwise -> Just (columnType (fromJust the_column))
+getColumnType name column_list =
+   let the_column = getColumnByName name column_list
+   in case the_column of
+      Nothing   -> Nothing
+      otherwise -> Just (columnType (fromJust the_column))
 
+alterColumnType :: String -> ColumnType -> [Column] -> [Column]
+alterColumnType name new_column_type column_list = map (alter_column_type name new_column_type) column_list
+
+alterColumnName :: String -> String -> [Column] -> [Column]
+alterColumnName name new_column_name column_list = map (alter_column_name name new_column_name) column_list
