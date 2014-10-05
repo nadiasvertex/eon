22f815d2f9a0a7efece17120e08527c7f59b558b
Christopher Nelson <nadiasvertex@gmail.com>
2014-10-05 07:36:38 -0400

    Added complex upper bound test.

--- engine/collection/tests/test_interval.py
+++ engine/collection/tests/test_interval.py
@@ -42,4 +42,21 @@ class TestStandardStore(unittest.TestCase):
 
         self.assertEqual([(j,j+9) for j in range(0, 1000, 20)], a)
 
+    def test_insert_complex_upper_bound(self):
+        a = []
+        for i in range(9, -1, -1):
+            Interval.insert(a, i)
+
+        self.assertEqual([(0, 9)], a)
+
+        for i in range(9, -1, -1):
+            Interval.insert(a, i)
+
+        self.assertEqual([(0, 9)], a)
+
+        for j in range(980, -1, -20):
+            for i in range(j+9, j-1, -1):
+                Interval.insert(a, i)
+
+        self.assertEqual([(j,j+9) for j in range(0, 1000, 20)], a)
 
