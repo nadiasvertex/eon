5dfa4f2a5a67412a6634c71f11239a0d61c23ece
Christopher Nelson <nadiasvertex@gmail.com>
2014-11-06 19:05:12 -0500

    Filled out datatype a little.

--- engine/schema/datatype.py
+++ engine/schema/datatype.py
@@ -1,5 +1,16 @@
 __author__ = 'Christopher Nelson'
 
+import enum
 
-class Integer:
-    pass
+
+class DataType(enum.Enum):
+    i8 = 0
+    i16 = 1
+    i32 = 2
+    i64 = 3
+    f32 = 4
+    f64 = 5
+    decimal = 6
+    varchar = 7
+
+
