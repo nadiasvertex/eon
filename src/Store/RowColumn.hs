d5897feb5b1e361df6b0f9d128ab2aca486de1b2
Christopher Nelson <csnelson@bethel.jw.org>
2015-02-20 16:54:40 -0500

    Started an updated method.

--- src/Store/RowColumn.hs
+++ src/Store/RowColumn.hs
@@ -1,28 +1,26 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module RowColumn where
 
 import           Control.Lens
 import           Data.Int            (Int64)
 import qualified Data.Map as Map
+import           Data.Maybe
 import qualified Data.Vector         as V
 import qualified Data.Vector.Unboxed as VU
 
 -- The value of rows
 
 data Row = Row {
-  _rid      :: Int64,           -- The row id.
-  _version  :: Int64,           -- The version of this row.
-  _columns  :: VU.Vector Int64, -- The addresses of the columns for this row.
-  _present  :: VU.Vector Bool   -- Which columns are present in this row.
+  rid      :: Int64,           -- The row id.
+  version  :: Int64,           -- The version of this row.
+  columns  :: VU.Vector Int64, -- The addresses of the columns for this row.
+  present  :: VU.Vector Bool   -- Which columns are present in this row.
 } deriving (Show)
-makeLenses ''Row
 
 instance Eq Row where
-  Row{_rid=r1, _version=v1} == Row{_rid=r2, _version=v2} = (r1==r2) && (v1==v2)
+  Row{rid=r1, version=v1} == Row{rid=r2, version=v2} = (r1==r2) && (v1==v2)
 
 instance Ord Row where
-  Row{_rid=r1, _version=v1} < Row{_rid=r2, _version=v2}
+  Row{rid=r1, version=v1} < Row{rid=r2, version=v2}
     | r1==r2    = v1<v2
     | r1<r2     = True
     | otherwise = False
@@ -30,16 +28,40 @@ instance Ord Row where
 -- The row column
 
 data RowColumn = RowColumn {
-  rows :: Map.Map Int64 Row
+  rows :: Map.Map Int64 [Row]
 }
 
-append RowColumn{rows=old_rows} rid version present values =
+append :: RowColumn          -> Int64 -> Int64 -> VU.Vector Bool -> VU.Vector Int64 -> RowColumn
+append RowColumn{rows=old_rows} rid      version  present           values =
    RowColumn{rows=new_rows}
    where
-      new_rows = Map.insert rid new_row old_rows
-      new_row  = Row{
-         _rid    =rid,
-         _version=version,
-         _columns=values,
-         _present=present
+      new_rows         = Map.insert rid new_row_versions old_rows
+      old_row_versions = fromMaybe [] (Map.lookup rid old_rows)
+
+      new_row_versions = new_row_version : old_row_versions
+      new_row_version  = Row{
+         rid    =rid,
+         version=version,
+         columns=values,
+         present=present
+      }
+
+update :: RowColumn          -> Int64 -> Int64         -> Int64 -> VU.Vector Bool -> VU.Vector Int64 -> RowColumn
+update RowColumn{rows=old_rows} rid      previous_version version  present           values =
+   RowColumn{rows=new_rows}
+   where
+      new_rows = Map.insert rid new_row_versions old_rows
+
+      match_row Row{version=current_version} =
+         current_version == previous_version
+
+      old_row_versions = fromMaybe [] (Map.lookup rid old_rows)
+      old_row_version  = head $ filter match_row old_row_versions
+
+      new_row_versions = new_row_version old_row_version : old_row_versions
+      new_row_version Row{columns=old_columns, present=old_present} = Row {
+         rid    =rid,
+         version=version,
+         columns=values,
+         present=present
       }
