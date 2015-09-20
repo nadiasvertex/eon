22d388edd68277c3587e0ab3059989328ded89e6
Christopher Nelson <nadiasvertex@gmail.com>
2015-09-20 12:37:08 -0400

    Add decoder generator.

--- eon/store/column.py
+++ eon/store/column.py
@@ -21,11 +21,12 @@ class Column:
         self.data_type = data_type
         self.data_dir = data_dir
         self.encoder_path = os.path.join(self.data_dir, "encode")
+        self.decoder_path = os.path.join(self.data_dir, "decode")
 
         if not os.path.exists(self.data_dir):
             os.makedirs(self.data_dir)
 
-    def _gen_write(self, p):
+    def _gen_write_value(self, p):
         if self.data_type == DataType.small_int:
             limit = 16
         elif self.data_type == DataType.standard_int:
@@ -41,11 +42,37 @@ class Column:
         values = ["(value>>%d) as u8" % i for i in range(0, limit, 8)]
         p.write(",".join(values))
         p.write("];\n")
-        p.write("f.write(&bytes).unwrap();\n");
+        p.write("f.write(&bytes).unwrap();\n")
+
+    def _gen_read_value(self, p):
+        if self.data_type == DataType.small_int:
+            limit = 16
+        elif self.data_type == DataType.standard_int:
+            limit = 32
+        elif self.data_type == DataType.big_int:
+            limit = 64
+        elif self.data_type == DataType.decimal:
+            limit = 64
+        elif self.data_type == DataType.bool:
+            limit = 8
+
+        p.write("let mut bytes = [0; %d];\n" % (limit / 8))
+        p.write("if f.read(&mut bytes).unwrap() == 0 {\n")
+        p.indent()
+        p.write('println!("");\nbreak;\n')
+        p.dedent()
+        p.write("}\n")
+
+        type_name = data_type_map[self.data_type]
+        values = ["((bytes[%d] as %s)<<%d)" % (i / 8, type_name, i) for i in range(0, limit, 8)]
+        p.write("let value = ")
+        p.write("|".join(values))
+        p.write(";\n")
+        p.write('print!("{} ", value);\n')
 
     def _make_encoder(self):
         """
-        An encoder takes a batch count and a list of values and converts it into binary
+        An encoder takes a list of values and converts it into binary
         values of the correct type. Those are written into a column data file.
         """
         p = IndentedWriter()
@@ -56,9 +83,6 @@ class Column:
 
         p.write("fn main() {\n")
         p.indent()
-        #p.write("let mut record_count_s = String::new();\n")
-        #p.write("io::stdin().read_line(&mut record_count_s).unwrap();\n")
-        #p.write("let record_count = record_count_s.parse::<u32>().unwrap();\n\n")
 
         p.write('let mut f = OpenOptions::new().write(true).append(true).create(true).open("test.array").unwrap();\n\n')
 
@@ -67,7 +91,7 @@ class Column:
         p.write('for v in data.split_whitespace() {\n')
         p.indent()
         p.write("let value = v.parse::<%s>().unwrap();\n" % data_type_map[self.data_type])
-        self._gen_write(p)
+        self._gen_write_value(p)
         p.dedent()
         p.write("}\n")
 
@@ -80,7 +104,46 @@ class Column:
 
         return os.system("rustc -O -o %s %s" % (self.encoder_path, encoder_src)) == 0
 
+    def _make_decoder(self):
+        """
+        An decoder takes a binary column data file and writes its values to stdout.
+        """
+        p = IndentedWriter()
+
+        p.write("use std::io::prelude::*;\n")
+        p.write("use std::fs::OpenOptions;\n\n")
+
+        p.write("fn main() {\n")
+        p.indent()
+
+        p.write('let mut f = OpenOptions::new().read(true).open("test.array").unwrap();\n\n')
+
+        p.write("loop {\n")
+        p.indent()
+
+        self._gen_read_value(p)
+        p.dedent()
+        p.write("}\n")
+
+        p.dedent()
+        p.write("}\n")
+
+        decoder_src = os.path.join(self.data_dir, "decode.rs")
+        with open(decoder_src, "w") as o:
+            o.write(p.getvalue())
+
+        return os.system("rustc -O -o %s %s" % (self.decoder_path, decoder_src)) == 0
+
     def get_encoder(self):
         if not os.path.exists(self.encoder_path):
             if not self._make_encoder():
                 return None
+
+        return self.encoder_path
+
+    def get_decoder(self):
+        if not os.path.exists(self.decoder_path):
+            if not self._make_decoder():
+                return None
+
+        return self.decoder_path
